bcad373b8164ac8855882340d9cd38205d8b9d37
LouisPrepmyfuture <97509071+LouisPrepmyfuture@users.noreply.github.com>
2022-06-09 13:22:36 +0200

    page index ok sauf l'accessibilité

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, country, city, tagline, price, id } = data;
     const picture = `assets/photographers/${portrait}`;
+    const link = `photographer.html?id=${id}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const a = document.createElement( 'a' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        const p =  document.createElement( 'p' );
+        const p2 =  document.createElement( 'p' );
+        const span =  document.createElement( 'span' );
         const h2 = document.createElement( 'h2' );
+
+
+        img.setAttribute("src", picture)
+        a.setAttribute("href", link)
+        
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        span.textContent = country + ", " + city;
+        p.textContent = tagline;
+        p2.textContent = price + "/jour";
+
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(span);
+        article.appendChild(p);
+        article.appendChild(p2);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,26 @@
+
+ async function getData() {
+    try{
+        let response = await fetch("http://127.0.0.1:5500/data/photographers.json")
+        if(response.ok){
+            let data = await response.json()
+            return data;
+        }else{
+            console.error('Retour du serveur :', response.status)
+        }
+    }catch(e){
+        console.log(e)
+    }
+} 
+
+
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const photographers = await getData()
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        
+        
+        return  photographers
     }
 
     async function displayData(photographers) {
@@ -40,6 +38,6 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
-    
+  
+
     init();
-    
